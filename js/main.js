ae0805d64885ec20838038e86bf027efa7c9cab8
Giuseppe <giuseppe.dagostino.d@gmail.com>
2020-12-09 17:53:56 +0100

    sezione bonus completata

--- js/main.js
+++ js/main.js
@@ -9,6 +9,15 @@ var app = new Vue(
       images: ["img/castelluccio.jpg","img/cascata.jpg","img/5terre.jpg","img/sardegna.jpg","img/trentino.jpg"],
     },
 
+    // sezione bonus
+    created: function() {
+      setInterval(
+        () => {
+          this.nextImage()
+        }, 2000
+      );
+    },
+
     methods: {
       nextImage: function() {
         this.imageIndex++;
