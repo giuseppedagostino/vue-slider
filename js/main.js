1cec787aae5fc7b3707453840f4a177939fecc0f
Giuseppe <giuseppe.dagostino.d@gmail.com>
2020-12-09 13:39:35 +0100

    Avvio progetto

---
+++ js/main.js
@@ -0,0 +1,2 @@
+// Provare a riprodurre uno slider di immagini, già fatto in precedenza con jQuery, questa volta con Vue, quindi non manipolando direttamente il DOM, ma i dati di Vue, come fatto stamattina insieme.
+// Bonus: aggiungiamo autoplay.
